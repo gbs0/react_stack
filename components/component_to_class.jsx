4f5daa8b53695fa54bd1037f91e5ae3b713a7a52
gabriel schiavo <gabriel@lewagon.org>
2019-03-27 13:00:48 -0300

    add class constructors and renders

--- components/component_to_class.jsx
+++ components/component_to_class.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React,  from 'react';
 import ReactDOM from 'react-dom';
 
 // import .scss file;
 
-// React component
-const Hello = (props) => {
+const Hello = (props) => {					// React component
 	return <div> Hello World!</div>;
 }
 
-// Component as class
-class Hello extends Component {
+class Hello extends Component {			// Component as class
+	constructor() {										// Must have a constructor method
 
+	}
+	render() {												// Must have a render method
+
+	}											
 }
 
 // locate DOM element for render
 const root = document.getElementById('root');
+// Render the element
+if (root) {
+	ReactDOM.render(
+		<Hello city="São Paulo" />, root);
+}
