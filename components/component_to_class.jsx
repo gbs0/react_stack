14bee18a1555ff3643d045a17559aab31e67172f
gabriel schiavo <gabriel@lewagon.org>
2019-03-27 13:32:06 -0300

    finish component

--- components/component_to_class.jsx
+++ components/component_to_class.jsx
@@ -1,18 +1,25 @@
-import React,  from 'react';
+// Import default component from react;
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 // import .scss file;
 
-const Hello = (props) => {					// React component
-	return <div> Hello World!</div>;
-}
+// const Hello = (props) => {					// React component
+// 	return <div> Hello World!</div>;
+// }
 
-class Hello extends Component {			// Component as class
-	constructor() {										// Must have a constructor method
-
+class Hello extends Component {			// Rewriting as class
+	constructor(props) {										// Must have a constructor method
+		super(props);
+		this.state = {
+			clicked: false
+		};
 	}
-	render() {												// Must have a render method
 
+	render() {												// Must have a render method for build and return HTML
+		return(
+			<div>Hello {this.props.city}</div
+		)
 	}											
 }
 
