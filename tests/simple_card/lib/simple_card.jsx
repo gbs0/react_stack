5f6995068c39e6dc5133f1eba3dda20d353e09e2
gabriel schiavo <gabriel@lewagon.org>
2019-04-01 13:38:42 -0300

    add test env

---
+++ tests/simple_card/lib/simple_card.jsx
@@ -0,0 +1,41 @@
+// TODO: update dependencies if needed
+// import React from 'react';
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+
+
+// TODO: listen to the click event on `.article`
+// TODO: code the `handleClick` function (which adds a 'clicked' class to the `.article`)
+
+// TODO: transform the following functional component into a class based component
+class Article extends Component {
+  constructor (props) {
+  	super(props);
+  
+  // TODO: set the initial state { clicked: false }
+  this.state = { clicked: false };
+	}
+
+	handleClick = () => {  // Fat arrow to reference self 
+		this.setState({			 // for this in state change
+			clicked: !this.state.clicked
+		});
+	}
+
+	render() {
+		return (
+	    <div className ={this.state.clicked ? "article clicked" : "article" }
+	    onClick={this.handleClick} >
+	    <h2 className="article-title" >
+	    My fancy new product
+      </h2>
+      <p>
+        Test it now!
+      </p>
+	    </div>
+	  );
+	}
+}
+
+// Export Class
+export default Article;
