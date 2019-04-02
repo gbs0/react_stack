24f6a925081237942584a809e6bb7415d616b66c
gabriel schiavo <gabriel@lewagon.org>
2019-04-01 21:06:42 -0300

    add render function

--- components/stateful_component.jsx
+++ components/stateful_component.jsx
@@ -7,6 +7,18 @@ class Name extends Component {
 		//Since we are extending the default constructor, 
 		//handle default activities first.
 		super(props);
+		
+		//Extract the first-name from the prop
+		const firstName = this.props.name.split(" ")[0];
 
+		//In the constructor, feel free to modify the
+		//state property on the current context. 
+		this.state = {
+    	name: firstName
+    }
+	} //Look maa, no comma required in JSX based class defs!
+
+	render() {
+		return <h1>Hello, {this.state.name}!</h1>
 	}
 }
