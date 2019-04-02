6888b09490b2645b032403704d545c47a9216ff3
gabriel schiavo <gabriel@lewagon.org>
2019-04-01 21:03:45 -0300

    Extract the first-name from the prop

--- components/stateful_component.jsx
+++ components/stateful_component.jsx
@@ -2,3 +2,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+class Name extends Component {
+	constructor(props) {
+		//Since we are extending the default constructor, 
+		//handle default activities first.
+		super(props);
+
+	}
+}
