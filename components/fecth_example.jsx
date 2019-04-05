f204fd6f713f0d6f2aa5eccc4f02c1814587768e
gabriel schiavo <gabriel@lewagon.org>
2019-04-05 14:54:33 -0300

    add fetch component example

---
+++ components/fecth_example.jsx
@@ -0,0 +1,14 @@
+// Import default component from react;
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class FetchClass extends React.Component {
+	// Add constructor
+	constructor() {
+		super()
+
+		this.state = {
+			user
+		}
+	}
+}
