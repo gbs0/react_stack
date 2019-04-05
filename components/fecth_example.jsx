319577c62d0353351f74f4c6189a1902b8462488
gabriel schiavo <gabriel@lewagon.org>
2019-04-05 15:08:43 -0300

    add xhr abort function

--- components/fecth_example.jsx
+++ components/fecth_example.jsx
@@ -5,10 +5,36 @@ import ReactDOM from 'react-dom';
 class FetchClass extends React.Component {
 	// Add constructor
 	constructor() {
-		super()
+		super();
 
 		this.state = {
-			user
+			user: {},
+			xhr: null
+		};
+	}
+	
+	componentWillUnmount() {
+		xhr = this.state.xhr;
+
+		// Cancel the xhr request, so the callback is never called
+		if (xhr && xhr.readyState != 4) {
+			xhr.abort();
 		}
+	}
+	// Check if component is mounted
+	componentDidMount() {
+		this.fetchUser();
+	}
+
+	// Asynchronous function
+	fetchUser() {
+		$.get('/api/users/self')
+			.then((user) => {
+				this.setState({user : user});
+			});
+	}
+
+	render() {
+		return <h1>{this.state.user}</h1>;
 	}
 }
