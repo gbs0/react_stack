e396ca792cade9dfeb8a1cb51dd30dd116ec3fce
gabriel schiavo <gabriel@lewagon.org>
2019-04-06 23:21:03 -0300

    add search var example

--- components/giphy_fetch.jsx
+++ components/giphy_fetch.jsx
@@ -5,7 +5,8 @@ import ReactDOM from 'react-dom';
 const api_key: "";
 const ranting = null;
 
-const xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+// Search example
+//const xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
 
 class FetchClass extends React.Component {
 	// Add constructor
